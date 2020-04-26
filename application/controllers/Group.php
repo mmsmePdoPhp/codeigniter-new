469c54425502e7a4d9c986f096418f18cbf18c6d
Mohammad Ravand <githubmohammadrepo@gmail.com>
2020-04-26 21:11:10 +0430

    package was completed added and using it

---
+++ application/controllers/Group.php
@@ -0,0 +1,339 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Group extends MY_Controller
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('group_model');
+		$this->load->helper('url_helper');
+		$this->load->library('session');
+		$this->load->library('form_validation');
+		$this->load->helper('url');
+
+	}
+
+	/**
+	 * get and show all groupss
+	 * atgrance
+	 * active
+	 * 
+	 */
+	public function index(String $tag = 'atglance')
+	{
+		$tag = trim($tag);
+
+		$re = "/[a-z]/";
+		$alpha = (preg_match($re, $tag));
+
+		$re = "/[^a-z]/";
+		$notAlphpa = (preg_match($re, $tag));
+
+		//filter length is not greter than 12
+		if ($alpha == true && $notAlphpa == false) {
+
+			// $this->dd($this->input->get(null,true));
+			switch ($tag) {
+				case 'atglance':
+					$data['groupss'] = ($this->group_model->get_groups($tag));
+					break;
+
+				case 'active':
+					$data['groupss'] = ($this->group_model->get_groups($tag));
+					break;
+
+				case 'notactive':
+					$data['groupss'] = ($this->group_model->get_groups($tag));
+					break;
+
+				case 'deleted':
+					$data['groupss'] = ($this->group_model->get_groups($tag));
+					break;
+
+				case 'fullinfo':
+					$data['groupss'] = ($this->group_model->get_groups($tag));
+					break;
+			}
+
+			$data['columns'] = ($this->db->list_fields('groups'));
+		} else {
+			echo 'false';
+		}
+		$this->loadhead();
+		$this->load->view('dashboard/groups/index', $data);
+		$this->loaddown();
+	}
+	public function ajaxindex(String $tag = 'atglance')
+	{
+		$tag = trim($tag);
+
+		$re = "/[a-z]/";
+		$alpha = (preg_match($re, $tag));
+
+		$re = "/[^a-z]/";
+		$notAlphpa = (preg_match($re, $tag));
+
+		//filter length is not greter than 12
+		if ($alpha == true && $notAlphpa == false) {
+
+			// $this->dd($this->input->get(null,true));
+			switch ($tag) {
+				case 'atglance':
+					$response =  ($this->group_model->get_groups($tag));
+					break;
+
+				case 'active':
+					$response =  ($this->group_model->get_groups($tag));
+					// $response = array('status' => 'OK');
+
+					
+					break;
+
+				case 'notactive':
+					$response =  ($this->group_model->get_groups($tag));
+					break;
+
+				case 'deleted':
+					$response =  ($this->group_model->get_groups($tag));
+					break;
+
+				case 'fullinfo':
+					$response =  ($this->group_model->get_groups($tag));
+				break;
+
+				case 'operation':
+					$response =  ($this->group_model->get_groups($tag));
+				break;
+			}
+
+			$this->output
+				->set_status_header(200)
+				->set_content_type('application/json', 'utf-8')
+				->set_output(json_encode($response, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES))
+				->_display();
+			exit;
+		} else {
+			echo 'false';
+		}
+	}
+	/**
+	 * get and show all groupss
+	 */
+	public function create()
+	{
+
+		$this->load->helper('form');
+		$this->load->library('form_validation');
+
+		$this->loadhead();
+		$this->load->view('dashboard/groups/create');
+		$this->loaddown();
+	}
+
+
+	/**
+	 * redirect to index page and show information inside it 
+	 * filtered by id 
+	 * 
+	 */
+	public function edit($tag)
+	{
+        $tag = trim($tag);
+
+        $re = "/[0-9]/";
+        $numeric = (preg_match($re, $tag));
+
+        $re = "/[^0-9]/";
+        $notNumeric = (preg_match($re, $tag));
+
+        //filter length is not greter than 12
+        if ($numeric == true && $notNumeric == false) {
+            if (($this->group_model->get_groups_byId($tag))) {
+                //data retured and i.e groups is exist in table
+                $data['groups'] = ($this->group_model->get_groups_byId($tag));
+            } else {
+                //if not exist redirect back
+				redirect('/groups/index', 'refresh');
+            }
+        } else {
+            echo 'false';
+        }
+        $this->loadhead();
+        $this->load->view('dashboard/groups/edit', $data);
+        $this->loaddown();
+    }
+	
+	
+	
+	/**
+	 * delete group == soft delete on table
+	 *  set deleted_at to current time
+	 * tag here is equal to => (groups.id)
+	 * get id group by post
+	 */
+	public function delete()
+	{
+		//first -> validate value
+        $this->form_validation->set_rules(
+			'id',
+			'id',
+			'required|numeric',
+			array(
+				'required'      => 'You have not provided %s.',
+				'numeric'      => 'delete group was denied!. %s.',
+			)
+		);
+
+		// $this->dd('is successed');
+
+        //filter length is not greter than 12
+		if (($this->group_model->delete_groups())) {
+			//redirect and flash messaage deleted
+			$this->session->set_flashdata('insert_success', 'groups was successfully deleted!');
+
+			redirect('/group/index', 'refresh');
+		} else {
+			//does not deleted row or Error accured...
+			$this->session->set_flashdata('insert_error', 'groups does not deleted!');
+
+			redirect('/group/index', 'refresh');
+		}
+
+
+	}
+	
+
+	/**
+	 * resotre group by id
+	 */
+	public function restore()
+	{
+		//first -> validate value
+        $this->form_validation->set_rules(
+			'id',
+			'id',
+			'required|numeric',
+			array(
+				'required'      => 'You have not provided %s.',
+				'numeric'      => 'delete group was denied!. %s.',
+			)
+		);
+
+		// $this->dd('is successed');
+
+        //filter length is not greter than 12
+		if (($this->group_model->restore_groups())) {
+			//redirect and flash messaage deleted
+			$this->session->set_flashdata('insert_success', 'groups was successfully restored!');
+
+			redirect('/group/index', 'refresh');
+		} else {
+			//does not deleted row or Error accured...
+			$this->session->set_flashdata('insert_error', 'groups does not restored!');
+
+			redirect('/group/index', 'refresh');
+		}
+
+
+    }
+
+
+	/**
+	 * store information new groups
+	 */
+	public function store()
+	{
+
+
+		$this->form_validation->set_rules(
+			'name',
+			'name',
+			'required|min_length[5]|max_length[22]|is_unique[groups.name]',
+			array(
+				'required'      => 'You have not provided %s.',
+				'is_unique'     => 'This %s already exists.'
+			)
+		);
+
+		$this->form_validation->set_rules(
+			'description',
+			'description',
+			'required|min_length[10]|max_length[100]',
+			array(
+				'required'      => 'You have not provided %s.',
+				'is_unique'     => 'This %s already exists.'
+			)
+		);
+
+		if ($this->form_validation->run() === FALSE) {
+			$this->loadhead();
+			$this->load->view('dashboard/groups/create');
+			$this->loaddown();
+		} else {
+			if ($this->group_model->set_groups()) {
+				//insert success
+
+				//flash session
+				$this->session->set_flashdata('insert_success', 'groups was successfully created!');
+
+				redirect('group/index', 'refresh');
+				
+			} else {
+				//show error 
+				log_message('error', 'Some variable did not contain a value.');
+				//redirect to create group
+				redirect('group/create', 'refresh');
+
+			}
+		}
+	}
+
+	public function update()
+	{
+		$this->form_validation->set_rules(
+			'groups',
+			'groups',
+			'required|min_length[5]|max_length[22]',
+			array(
+				'required'      => 'You have not provided %s.',
+				'is_unique'     => 'This %s already exists.'
+			)
+		);
+
+		$this->form_validation->set_rules(
+			'id',
+			'id',
+			'required|min_length[1]|max_length[20]',
+			array(
+				'required'      => 'You have not provided %s.',
+			)
+		);
+
+		if ($this->form_validation->run() === FALSE) {
+			$this->loadhead();
+			$this->load->view('dashboard/groups/create');
+			$this->loaddown();
+		} else {
+			if ($this->group_model->update_groups()) {
+				//insert success
+
+				//flash session
+				$this->session->set_flashdata('insert_success', 'groups was successfully updated!');
+
+				redirect('/groups/index', 'refresh');
+
+			} else {
+				//set flahMessage 
+				$this->session->set_flashdata('insert_error', 'groups wasn`t updated!');
+
+				//redirect to create groups
+				redirect('/groups/edit/'.$this->input->post('id'), 'refresh');
+
+			}
+		}
+	}
+
+	
+}
---
+++ application/models/group_model.php
@@ -0,0 +1,204 @@
+<?php
+
+use Carbon\Traits\Timestamp;
+
+class Group_model extends MY_Model
+{
+	
+	
+	public function __construct()
+	{
+		$this->now = date('Y-m-d h:m:s',time());
+		$this->load->database();
+		$this->load->helper('url');
+		$this->load->helper('date');
+
+
+	}
+
+	/***
+	 * set new user type in database if not exists
+	 */
+	public function set_groups()
+	{
+		
+		$data = array(
+			'name' => $this->input->post('name'),
+			'description' => $this->input->post('description'),
+			'isactive' => $this->input->post('isactive') == 'on' ? 1 : 0,
+			'created_at' => $this->now
+		);
+
+		//if group not exsit insert
+		$query = $this->db->get_where('groups', array('name' => $data['name']));
+		if ($query->row_array()) {
+			//show errow
+			return false;
+		} else {
+			//insert data
+			return $this->db->insert('groups', $data);
+		}
+	}
+	
+	/***
+	 * set new user type in database if not exists
+	 */
+	public function update_groups()
+	{
+		$data = array(
+			'id' => $this->input->post('id'),
+			'groups' => $this->input->post('groups'),
+			'isactive' => $this->input->post('isactive') == 'on' ? 1 : 0,
+		);
+
+		//if groups not exsit insert
+		$query = $this->db->get_where('groups', array('id' => $data['id']));
+		if ($query->row_array()) {
+			//if exist update 
+			return $this->db->replace('groups', $data);
+			
+		} else {
+			return false; // return false for handle error and redirect user
+		}
+	}
+
+
+	/***
+	 * soft delete group 
+	 */
+	public function delete_groups()
+	{
+		$data = array(
+			'id' => $this->input->post('id'),
+		);
+
+		//if groups not exsit insert
+		$query = $this->db->get_where('groups', array('id' => $data['id']));
+		if ($query->row_array()) {
+			//get now timestamp
+			$now =date('Y-m-d h:m:s',now());
+			
+			//soft delete row
+			$data = array(
+				'id' => $data['id'],
+				'now'  => date($now)
+			);
+			
+			
+			$this->db->set('deleted_at',$data['now'], true);
+			$this->db->where('id', $data['id']);
+			return $this->db->update('groups');
+		
+		// Executes: REPLACE INTO mytable (title, name, date) VALUES ('My title', 'My name', 'My date')
+			
+		} else {
+			return false; // return false for handle error and redirect user
+		}
+	}
+
+	/***
+	 * soft delete group 
+	 */
+	public function restore_groups()
+	{
+		$data = array(
+			'id' => $this->input->post('id'),
+		);
+
+		//if groups not exsit insert
+		$query = $this->db->get_where('groups', array('id' => $data['id']));
+		if ($query->row_array()) {
+			//get now timestamp
+			
+			//soft delete row
+			$data = array(
+				'id' => $data['id'],
+			);
+			
+			
+			$this->db->set('deleted_at',null, true);
+			$this->db->where('id', $data['id']);
+			return $this->db->update('groups');
+		
+		// Executes: REPLACE INTO mytable (title, name, date) VALUES ('My title', 'My name', 'My date')
+			
+		} else {
+			return false; // return false for handle error and redirect user
+		}
+	}
+
+	/**
+	 * get all groupss
+	 */
+	public function get_groups($tag)
+	{
+		switch ($tag) {
+			case 'atglance':
+				$this->db->select('id,name,description,isActive,created_at');
+				$this->db->where('deleted_at IS Null'); // where group does not deleted
+
+			break;
+
+			case 'active':
+				$this->db->select('id,description,isActive,created_at,updated_at,deleted_at');
+				$this->db->where('deleted_at IS Null'); // where group does not deleted
+				
+				$this->db->where('isActive', 1); // where group is active
+
+			break;
+
+			case 'notactive':
+				$this->db->select('id,description,isActive,created_at,updated_at,deleted_at');
+				$this->db->where('deleted_at IS Null'); // where group does not deleted
+
+				$this->db->where('isActive', 0); // where group is not active
+
+			break;
+
+			case 'deleted':
+				$this->db->select('id,description,isActive,deleted_at');
+				$this->db->where('deleted_at IS NOT NULL'); // Produces: WHERE name = 'Joe'
+
+			break;
+
+			case 'fullinfo':
+				$this->db->select('id,description,isActive,created_at,updated_at,deleted_at');
+			break;
+			
+			case 'operation':
+				$this->db->select('id,description');
+
+			break;
+			
+		}
+			$query = $this->db->get('groups');
+
+		if (count($query->result())) {
+			//show information
+
+			return $query->result();
+		} else {
+			//return false for controll logic
+			return false;
+		}
+	}
+	
+	/**
+	 * get one  groupss by its id
+	 */
+	public function get_groups_byId($id)
+	{
+		$this->db->select('*');
+		$this->db->where('id',$id); // Produces: WHERE name = 'Joe'
+
+		$query = $this->db->get('groups');
+
+		if (count($query->result())) {
+			//show information
+			return $query->result();
+		} else {
+			//return false for controll logic
+			return false;
+		}
+	}
+}
